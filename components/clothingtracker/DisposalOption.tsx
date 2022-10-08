a01a58da952e7b488fdcd73a9110e67465224b64
Matthew Elias <matthew.e.private@gmail.com>
2022-10-09 01:12:29 +1100

    Responsive disposal tool

--- components/clothingtracker/DisposalOption.tsx
+++ components/clothingtracker/DisposalOption.tsx
@@ -1,11 +1,32 @@
 import React from "react"
 import styles from "@/styles/components/clothingtracker/DisposalOption.module.scss"
+import { Box, Typography } from "@mui/material"
 
 export default function DisposalOption({ title, description }) {
   return (
-    <div className={styles.option}>
-      <span className={styles.title}>{title}</span>
-      <span className={styles.description}>{description}</span>
-    </div>
+    <Box
+      sx={{
+        textAlign: "center",
+        backgroundColor: "white",
+        borderRadius: "0.5em",
+        boxShadow: "1px 1px 2px 1px #888888",
+        padding: "0.75em",
+        cursor: "pointer",
+        transition: "all 200ms ease",
+        "&:hover": {
+          color: "white",
+          backgroundColor: "black",
+          boxShadow: "1px 1px 2px 1px #2c2c2c",
+          ".title": {
+            color: "#0ac05e",
+          },
+        },
+      }}
+    >
+      <Typography variant="h6" className="title">
+        {title}
+      </Typography>
+      <Typography variant="body1">{description}</Typography>
+    </Box>
   )
 }
--- components/clothingtracker/DisposalOptionGroupContainer.tsx
+++ components/clothingtracker/DisposalOptionGroupContainer.tsx
@@ -1,8 +1,26 @@
+import { Box, useMediaQuery } from "@mui/material"
 import React from "react"
 import DisposalOption from "./DisposalOption"
 
 export default function DisposalOptionGroupContainer({ options }) {
-  return options.map((x: any, index: number) => (
-    <DisposalOption key={index} title={x.title} description={x.description} />
-  ))
+  const isMobile = useMediaQuery("(max-width:768px)")
+
+  return (
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: !isMobile ? "1fr 1fr 1fr" : "1fr",
+        columnGap: "1em",
+        rowGap: "1em",
+      }}
+    >
+      {options.map((x: any, index: number) => (
+        <DisposalOption
+          key={index}
+          title={x.title}
+          description={x.description}
+        />
+      ))}
+    </Box>
+  )
 }
